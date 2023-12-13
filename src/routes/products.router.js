473baec0c8d27b9d0349e7f4b50ebf8918b0ae4c
Mackinlay Kevin <kemack83@gmail.com>
2023-12-12 23:17:53 -0300

    implementacion de router.put en products.router.js

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
     let response = await productManager.getProducts();
 
     if (limit) {
-      tempArray = response.filter((dat, index) => index < limit);
+      let tempArray = response.filter((dat, index) => index < limit);
 
       res.json({
         data: tempArray,
@@ -44,25 +44,42 @@ router.get("/:pid", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const { title, description, price, thumbnail, code, stock } = req.body;
-  try{
-  
- const result = await productManager.addProduct(
-    title,
-    description,
-    price,
-    thumbnail,
-    code,
-    stock
-  );
- res.json({ message: "success", data:result });
-  }catch(err){
+  try {
+    const result = await productManager.addProduct(title, description, price, thumbnail, code, stock);
+    res.json({ message: "success", data: result });
+  } catch (err) {
     console.log(err);
     res.status(500).json({ message: "error", data: err });
   }
 });
 
-router.put("/", (req, res) => {
-  res.send("hola mundo desde router de user");
+router.put("/:pid", async (req, res) => {
+  const { pid } = req.params;
+  const { title, description, price, thumbnail, code, stock } = req.body;
+  try {
+    let product = await productManager.getProductById(pid);
+
+    if (product) {
+      let newProduct = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price,
+        thumbnail: thumbnail || product.thumbnail,
+        code: code || product.code,
+        stock: stock || product.stock,  
+      }
+      const respuesta = await productManager.updateProductById(pid, newProduct);
+      res.json({ message: "success", data: respuesta });
+    } else {
+      res.json({
+        message: "el producto que solicitas no existe",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({ message: "error", data: err });
+  }
 });
 
 router.delete("/", (req, res) => {
