fbd9a3fce2cd105133fbe7261af491736178547c
Mackinlay Kevin <kemack83@gmail.com>
2023-12-20 17:57:14 -0300

    terminando desafio

--- src/app.js
+++ src/app.js
@@ -1,45 +1,64 @@
 import express from "express";
-//import { ProductManager } from "./classes/ProductManager.js";
+import { Server } from "socket.io";
 import productRouter from "./routes/products.router.js";
 import cartRouter from "./routes/cart.router.js";
-import { _dirname } from "./utils.js";
 import handlebars from "express-handlebars";
-import { Server } from "socket.io";
-import viewsRouter from "./routes/views.route.js";
+import viewsRouter from "./routes/views.router.js";
+import { _dirname } from "./utils.js";
+import { ProductManager } from "./classes/ProductManager.js";
 
 const app = express();
 const PORT = 8080;
+const productManager = new ProductManager("products.json");
 
-const httpServer = app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-}); //levanto el servidor http con socket io  incorporado en el mismo servidor        
-
-const socketServer = new Server(httpServer);  
-let products = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(_dirname + "/public"));
 
 app.engine("handlebars", handlebars.engine());
 app.set("views", _dirname + "/views");
 app.set("view engine", "handlebars");
 
-
-
-//const productManager = new ProductManager("./products.json");
-
-
-// app.get("/", (req, res) => {
-//   res.send("hola mundo");
-// });
-
-
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/", viewsRouter);
 
+const server = app.listen(PORT, () => {
+  console.log("servidor esta running en el puerto" + PORT);
+});
+
+const socketServer = new Server(server);
 
 
-// app.listen(PORT, () => {
-//   console.log(`Servidor escuchando en http://localhost:${PORT}`);
-// });
+socketServer.on("connection", (socket) => {
+  console.log("nuevo cliente conectado");
+  socket.on("addProduct", async (product) => {
+    const title = product.title;
+    const description = product.description;
+    const price = product.price;
+    const thumbnail = product.thumbnail;
+    const code = product.code;
+    const stock = product.stock;
+    try {
+      const result = await productManager.addProduct(title, description, price, thumbnail, code, stock);
+      const allProducts = await productManager.getProducts();
+      console.log(allProducts);
+      result && socketServer.emit("updateProducts", allProducts);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  socket.on("deleteProduct", async (id) => {
+    console.log(id);
+    try {
+      const result = await productManager.deleteProductById(id);
+      const allProducts = await productManager.getProducts();
+      console.log(allProducts);
+      result && socketServer.emit("updateProducts", allProducts);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
