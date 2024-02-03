b2f6b5ec4111c8f0cf1724106c358dc912f4c50d
Mackinlay Kevin <kemack83@gmail.com>
2024-02-03 19:06:21 -0300

    aplicando sessions mongoDb

--- src/app.js
+++ src/app.js
@@ -2,45 +2,61 @@ import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-import  FileStore  from "session-file-store";
+import FileStore from "session-file-store";
+import MongoStore from "connect-mongo";
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
 import IndexRouter from "./routes/index.routes.js";
+import dotenv from "dotenv";
 
-const fileStorage = FileStore(session);
-
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/";
 
 app.use(cookieParser());
 
+// esto es para que se guarde la sesion en la base de datos session-file-store
+// const fileStorage = FileStore(session);
+// app.use(
+//   session({
+//     store: new fileStorage({
+//       path: "./sessions",
+//       ttl: 100,
+//       retries: 0,
+//     }),
+//     secret: "codersecret",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+
+
 app.use(
   session({
-    store: new fileStorage({
-      path: "./sessions",
-      ttl: 100,
-      retries: 0,
+    store: MongoStore.create({
+      mongoUrl: DB_URL,
+      ttl: 15,
+      mongoOptions: {
+        useNewUrlParser: true,
+      },
     }),
     secret: "codersecret",
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
   })
 );
 
 app.get("/", (req, res) => {
-if (req.session.counter) {
-  req.session.counter++;
-  res.send(`Contador: ${req.session.counter}`);
-}else{
-  req.session.counter = 1;
-  res.send("Bienvenido");
-
-}
-// res.send("Hello World");
+  if (req.session.counter) {
+    req.session.counter++;
+    res.send(`Counter: ${req.session.counter}`);
+  } else {
+    req.session.counter = 1;
+    res.send("Bienvenido");
+  }
 });
 
-// app.listen("8080", () => console.log(object));
 
 
 app.use(express.json());
@@ -50,8 +66,6 @@ app.use(express.static("src/public"));
 app.engine("handlebars", handlebars.engine());
 app.set("views", "src/views");
 app.set("view engine", "handlebars");
-
-
 
 app.use((req, res, next) => {
   req.io = io;
