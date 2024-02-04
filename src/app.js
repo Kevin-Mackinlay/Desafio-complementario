5521f0d78d3ae65f9cbd1da120f6187690dfd3c0
Mackinlay Kevin <kemack83@gmail.com>
2024-02-04 00:52:20 -0300

    login, signup terminados, falta redireccion a products

--- src/app.js
+++ src/app.js
@@ -8,23 +8,25 @@ import { Server } from "socket.io";
 import handlebars from "express-handlebars";
 import IndexRouter from "./routes/index.routes.js";
 import dotenv from "dotenv";
-import {__dirname} from "../src/utils.js";
+import { __dirname } from "../src/utils.js";
 import loginRouter from "./routes/login.routes.js";
 import signupRouter from "./routes/signup.routes.js";
+import sessionRouter from "./routes/session.routes.js";
 
 dotenv.config();
 const app = express();
 const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/";
 const PORT = process.env.PORT || 8080;
-const COOCKIESECRET = process.env.CODERSECRET
+const COOCKIESECRET = process.env.CODERSECRET;
 
 //middlewares para el manejo de datos
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(cookieParser(COOCKIESECRET));
+
 app.use(express.static("src/public"));
+
 app.use("/", IndexRouter);
 
 app.engine("handlebars", handlebars.engine());
@@ -32,29 +34,31 @@ app.set("views", "src/views");
 app.set("view engine", "handlebars");
 
 //routes
-app.use("/login", loginRouter);
-app.use("/signup", signupRouter);
+
 
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
 
-
 app.use(
   session({
     store: MongoStore.create({
       mongoUrl: DB_URL,
-      ttl: 15,
       mongoOptions: {
         useNewUrlParser: true,
       },
+      ttl: 600,
     }),
     secret: "COOCKIESECRET",
     resave: false,
     saveUninitialized: true,
   })
 );
+app.use("/", sessionRouter);
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
+
 
 const environment = async () => {
   try {
@@ -68,11 +72,10 @@ const environment = async () => {
 environment();
 
 const server = app.listen(PORT, () => {
- console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 server.on("error", (error) => console.log(error));
-
 
 const io = new Server(server);
 
@@ -115,15 +118,11 @@ async function startMongoConnection() {
   await mongoose.connect(DB_URL);
 }
 
-
-
-
 // const server = app.listen(PORT, (error) => {
 //   if (error) {
 //     console.log(error);
 //   }
 //   console.log(`Servidor corriendo en el puerto ${PORT}`);
 // });
-
 
 //
--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const userCollection = "users";
 
 const UserSchema = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    role:{type: Number , required: true}
+    first_name: { type: String, require: true },
+    last_name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    age: { type: Number, require: true },
+    role:{type: String , require: true}
 });
 
 const User = mongoose.model(userCollection, UserSchema);
