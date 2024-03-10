c308dcfedcf290afd23bef737c89085afe150d5a
Mackinlay Kevin <kemack83@gmail.com>
2024-03-09 21:16:20 -0300

    working on repository

---
+++ src/dao/DTO/contact.js
@@ -0,0 +1,6 @@
+export default class ContactDTO{
+    constuctor(contact){
+        this.name = contact.name + " " + contact.lastName ;
+        this.phone = contact.phone ? contact.phone.split("-").join("") : "";
+    }
+}
--- src/routes/contacts.routes.js
+++ src/routes/contacts.routes.js
@@ -1,55 +1,120 @@
 import { Router } from "express";
+import ContactRepository from "../repository/Contacts.repository.js";
+// import ContactDTO from "../dao/DTO/contact.js";
 // import Contacts from "../dao/mongo/contact.mongo.js";
 // import Contacts from "../dao/memory/contact.memory.js";
-import Contacts from "../dao/factory.js";
+// import Contacts from "../dao/factory.js";
+
+
 
 const router = Router();
-const contacts = new Contacts();
+// const contacts = new Contacts();
+// const contactRepository = new ContactRepository(contacts);
+
 
 router.get("/", async (req, res) => {
-  try {
-    const data = await contacts.get();
-    res.json(data);
-  } catch (err) {
-   console.log("Error getting contacts", err);
+  try{
+    const data = await contactRepository.getContacts();
+    res.json(data); 
+  }
+  catch (err) {
+    console.error("Error getting contacts", err);
     res.status(500).json({ message: "Error getting contacts" });
-
   }
 });
 
-router.post("/", async (req, res) => {
-  try {
+  router.post("/", async (req, res) => {
+    try{
     const contact = req.body;
-    const newContact = new Contacts(contact);
-    const data = await contacts.create(newContact);
+    const data = await contactRepository.createContact(contact);
     res.json(data);
-  } catch (err) {
-    console.error("Error creating contact", err);
-    res.status(500).json({ message: "Error creating contact" });
-  }
-});
+    }
+    catch (err) {
+      console.error("Error creating contact", err);
+      res.status(500).json({ message: "Error creating contact" });
+    }
+  });
+ 
+  router.put("/:id", async (req, res) => {
+    try{
+      const id = req.params.id;
+      const contact = req.body
+      const data = await contactRepository.modifyContact(id, contact);
+      res.json(data);
+    }
+    catch (err) {
+      console.error("Error updating contact", err);
+      res.status(500).json({ message: "Error updating contact" });
+    }
+  });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const contact = req.body;
-    const data = await contacts.modify(id, contact);
-    res.json(data);
-  } catch (err) {
-    console.error("Error updating contact", err);
-    res.status(500).json({ message: "Error updating contact" });
-  }
-});
+  router.delete("/:id", async (req, res) => {
+    try{
+      const id = req.params.id;
+      const data = await contactRepository.deleteContact(id);
+      res.json(data);
 
-router.delete("/:id", async (req, res) => {
-    try {
-    const id = req.params.id;
-    const data = await contacts.delete(id);
-    res.json(data);
-  } catch (err) {
-    console.error("Error deleting contact", err);
-    res.status(500).json({ message: "Error deleting contact" });
-  }
-});
+    }
+    catch (err) {
+      console.error("Error deleting contact", err);
+      res.status(500).json({ message: "Error deleting contact" });
+    }
+  });
 
+    
 export default router;
+
+
+
+
+
+
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const data = await contacts.get();
+//     res.json(data);
+//   } catch (err) {
+//    console.log("Error getting contacts", err);
+//     res.status(500).json({ message: "Error getting contacts" });
+
+//   }
+// });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const contact = req.body;
+    
+//     const newContact = new ContactDTO(contact);
+//     const data = await contacts.create(newContact);
+//     res.json(data);
+//   } catch (err) {
+//     console.error("Error creating contact", err);
+//     res.status(500).json({ message: "Error creating contact" });
+//   }
+// });
+
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const contact = req.body;
+//     const data = await contacts.modify(id, contact);
+//     res.json(data);
+//   } catch (err) {
+//     console.error("Error updating contact", err);
+//     res.status(500).json({ message: "Error updating contact" });
+//   }
+// });
+
+// router.delete("/:id", async (req, res) => {
+//     try {
+//     const id = req.params.id;
+//     const data = await contacts.delete(id);
+//     res.json(data);
+//   } catch (err) {
+//     console.error("Error deleting contact", err);
+//     res.status(500).json({ message: "Error deleting contact" });
+//   }
+// });
+
+// export default router;
