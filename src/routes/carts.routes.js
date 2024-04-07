b8514ea2a18509093a4c52a6a39e79a7e62e2c90
Mackinlay Kevin <kemack83@gmail.com>
2024-04-06 22:09:12 -0300

    resolviendo errores

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,17 +1,17 @@
 import express from "express";
-import passportCall from "../passportJwt/passportCall.js";
+import {passportCall} from "../passportJwt/passportCall.js";
 import CartController from "../controllers/carts.controller.js";
 import authorization from "../passportJwt/authorization.js";
 
 const cartsRouter = express.Router();
 const cartsController = new CartController();
 
-cartsRouter.post("/", authorization(["admin"]), cartsController.createCart);
-cartsRouter.get("/", authorization(["admin"]), cartsController.getCarts);
-cartsRouter.get("/:cid", authorization(["admin"]), cartsController.getCartById);
-cartsRouter.post("/:cid/product/:pid", authorization(["admin"]), cartsController.addAndUpdate);
-cartsRouter.delete("/:cid/product/:pid", authorization(["admin"]), cartsController.deleteOne);
-cartsRouter.delete("/:cid", authorization(["admin"]), cartsController.deleteAll);
+cartsRouter.post("/", passportCall("jwt"), authorization(["admin"]), cartsController.createCart);
+cartsRouter.get("/",passportCall("jwt"), authorization(["admin"]), cartsController.getCarts);
+cartsRouter.get("/:cid", passportCall("jwt"),  cartsController.getCartById);
+cartsRouter.post("/:cid/product/:pid", passportCall("jwt"), authorization(["admin"]), cartsController.addProduct);
+cartsRouter.delete("/:cid/product/:pid", passportCall("jwt"), authorization(["admin"]), cartsController.deleteProductInCart);
+cartsRouter.delete("/:cid", passportCall("jwt"), authorization(["admin"]), cartsController.deleteProductsInCart);
 
 export default cartsRouter;
 
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,15 +1,15 @@
 import express from "express";
-import passportCall  from "../passportJwt/passportCall.js";
+import {passportCall}  from "../passportJwt/passportCall.js";
 import ProductsController from "../controllers/products.controller.js";
 import authorization from "../passportJwt/authorization.js";
 
 const productsRouter = express.Router();
 const productsController = new ProductsController();
 
-productsRouter.get("/", authorization(["admin"]), productsController.getProducts);
-productsRouter.get("/:pid", authorization(["admin"]), productsController.getProductById);
-productsRouter.post("/", authorization(["admin"]), productsController.createProduct);
-productsRouter.put("/:pid", authorization(["admin"]), productsController.updateProduct);
-productsRouter.delete("/:pid", authorization(["admin"]), productsController.deleteProduct);
+productsRouter.get("/", passportCall("jwt") , productsController.getProducts);
+productsRouter.get("/:pid", passportCall("jwt"), productsController.getProductById);
+productsRouter.post("/",passportCall("jwt"), authorization(["admin"]), productsController.createProduct);
+productsRouter.put("/:pid",passportCall("jwt"), authorization(["admin"]), productsController.updateProduct);
+productsRouter.delete("/:pid",passportCall("jwt"),  authorization(["admin"]), productsController.deleteProduct);
 
 export default productsRouter;
