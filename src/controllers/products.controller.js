a2bd89169161095eacd21a1b2608b487029c26e3
Mackinlay Kevin <kemack83@gmail.com>
2024-04-30 23:58:42 -0300

    realizando modulos de testing

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -24,9 +24,11 @@ export default class ProductsController {
       if (sort) {
         filter.options.sort = { price: sort };
       }
-      const pagesData = await this.productsService.getPaginatedProducts(filter);
+      console.log(1);
+      const pagesData = await this.productsService.getProducts(filter);
 
-      if (pagesData.products.length < 1) {
+      if (pagesData.docs.length < 1) {
+        console.log(2);
         res.status(404).json({
           success: false,
           message: "Could not retrieve products",
@@ -34,13 +36,15 @@ export default class ProductsController {
         return;
       }
 
-      //console.log(pagesData);
+      console.log(2); //console.log(pagesData);
+
       res.status(200).json({
         success: true,
         data: pagesData,
       });
     } catch (error) {
-      logger.error(error);
+      console.log(error);
+      req.logger.error(error);
     }
   };
 
@@ -63,7 +67,7 @@ export default class ProductsController {
         data: product,
       });
     } catch (error) {
-      logger.error(error);
+      req.logger.error(error);
     }
   };
 
@@ -84,97 +88,95 @@ export default class ProductsController {
       }
 
       //validacion si el code del producto ya existe
-      if(await this.productsService.getProduct({code})){
+      if (await this.productsService.getProduct({ code })) {
         CustomError.createError({
           name: "Error creating product",
-          cause: generateInfoProductError({title, description, price, thumbnails, stock}),
+          cause: generateInfoProductError({ title, description, price, thumbnails, stock }),
           message: "Existing code error",
           code: typeErrors.INVALID_TYPE_ERROR,
-      })
-    } 
-    let result = await this.productsService.createProduct({ title, description, price, thumbnails, stock, code, owner });
-    result
-       ? res.status(200).send({ status: "A product has been created successfully", payload: result })
-            : res.status(404).send({ status:"Error", error: "Something went wrong" })
-   } catch (error) {
-    next(error);
-      logger.error(error);
+        });
+      }
+      let result = await this.productsService.createProduct({
+        title,
+        description,
+        price,
+        thumbnails,
+        stock,
+        code,
+        owner,
+      });
+      result ? res.status(200).send({ status: "A product has been created successfully", payload: result }) : res.status(404).send({ status: "Error", error: "Something went wrong" });
+    } catch (error) {
+      next(error);
+      req.logger.error(error);
     }
   };
 
   updateProduct = async (req, res) => {
-   try{
-    let { pid } = req.params;
-    let updateBody = req.body;
-    const product = await this.productsService.getProductById({_id: pid})
+    try {
+      let { pid } = req.params;
+      let updateBody = req.body;
+      const product = await this.productsService.getProductById({ _id: pid });
 
-    if(!product) return
-      res.status(404).send({ status: "Error", error: "Product not found" })
-    
-      const updateProduct = async(pid, updateBody) => {
-        await this.productsService.updateProductById(pid, updateBody)
-        res.status(200).send({ status: "Product has been updated successfully",
-        message:`The product was updated ${product.title}`
-       })
-   }
-if(req.user.role === "premium") {
-  req.user.email !== product.owner ?
-   res.status(403).send({ status: "Error",
-    message: "You are not allowed to update this product" }) : updateProduct(pid, updateBody)
-}
-else {
-  updateProduct(pid, updateBody)
-}
+      if (!product) return;
+      res.status(404).send({ status: "Error", error: "Product not found" });
 
-}
-catch(error) {
-  logger.error(error);
-}
+      const updateProduct = async (pid, updateBody) => {
+        await this.productsService.updateProductById(pid, updateBody);
+        res.status(200).send({
+          status: "Product has been updated successfully",
+          message: `The product was updated ${product.title}`,
+        });
+      };
+      if (req.user.role === "premium") {
+        req.user.email !== product.owner ? res.status(403).send({ status: "Error", message: "You are not allowed to update this product" }) : updateProduct(pid, updateBody);
+      } else {
+        updateProduct(pid, updateBody);
+      }
+    } catch (error) {
+      req.logger.error(error);
+    }
   };
 
+  deleteProduct = async (req, res) => {
+    try {
+      let { pid } = req.params;
+      let product = await this.productsService.getProductById({ _id: pid });
 
+      if (!product) {
+        return res.status(404).send({ status: "Error", error: "Product not found" });
+      }
 
-
-deleteProduct = async (req, res) => {
-  try {
-    let { pid } = req.params;
-    let product = await this.productsService.getProductById({ _id: pid });
-
-    if (!product) {
-      return res.status(404).send({ status: "Error", error: "Product not found" });
-    }
-
-    const removeProduct = async (pid) => {
-      await productsService.deleteProductById(pid);
-      res.status(200).send({
-        status: "Product has been deleted successfully",
-        message: `The product was deleted ${product.title}`
-      });
-    };
-
-    if (req.user.role === "premium") {
-      if (req.user.email !== product.owner) {
-        return res.status(403).send({
-          status: "Error",
-          message: "You are not allowed to delete this product"
+      const removeProduct = async (pid) => {
+        await productsService.deleteProductById(pid);
+        res.status(200).send({
+          status: "Product has been deleted successfully",
+          message: `The product was deleted ${product.title}`,
         });
+      };
+
+      if (req.user.role === "premium") {
+        if (req.user.email !== product.owner) {
+          return res.status(403).send({
+            status: "Error",
+            message: "You are not allowed to delete this product",
+          });
+        } else {
+          removeProduct(pid);
+        }
       } else {
         removeProduct(pid);
-      }
-    } else {
-      removeProduct(pid);
-      await transport.sendMail({
-        from: process.env.EMAIL,
-        to: product.owner,
-        subject: "Product deleted",
-        html: `<div>
+        await transport.sendMail({
+          from: process.env.EMAIL,
+          to: product.owner,
+          subject: "Product deleted",
+          html: `<div>
           <h1>Hi user, your product was removed by the admin</h1>
-        </div>`
-      });
+        </div>`,
+        });
+      }
+    } catch (error) {
+      req.logger.error(error);
     }
-  } catch (error) {
-    logger.error(error);
-  }
-};
-
+  };
 }
